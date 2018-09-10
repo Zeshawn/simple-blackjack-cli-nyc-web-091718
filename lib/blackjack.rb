f3e94c2879ece23ce41def07ccc85eca8b1a10c5
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-10 23:23:48 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,7 +24,7 @@ def get_user_input
   letter = gets.chomp
 end
 
-def end_game(number)
+def end_game(card_total)
   # code #end_game here
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
   
